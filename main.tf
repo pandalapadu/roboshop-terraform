2624bc856d748acfbe5598e46c8d403cada68ab0
VenkatReddy Kunam <venkat@gmail.com>
2026-01-03 16:54:14 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -5,3 +5,52 @@ module "components" {
   vm_size = each.value["vm_size"]
   env = var.env
 }
+
+components = {
+  default = {
+    frontend = {
+      name        = "frontend"
+      vm_size     = "Standard_D2s_v3"
+    }
+    # mongodb = {
+    #   name        = "mongodb"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # catalogue = {
+    #   name        = "catalogue"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # user = {
+    #   name        = "user"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # cart = {
+    #   name        = "cart"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # mysql = {
+    #   name        = "mysql"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # shipping = {
+    #   name        = "shipping"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # payment = {
+    #   name        = "payment"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # rabbitmq = {
+    #   name        = "rabbitmq"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # redis = {
+    #   name        = "redis"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+    # dispatch = {
+    #   name        = "dispatch"
+    #   vm_size     = "Standard_D2s_v3"
+    # }
+  }
+}
