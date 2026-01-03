c91b0143d46bdf6342421b9c36b70e8604ae9f92
VenkatReddy Kunam <venkat@gmail.com>
2026-01-03 16:48:23 +0530

    Terraform

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.49.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "af504235-2f6d-4469-aa25-251f498730fc"
+}
+terraform {
   backend "azurerm" { }
 }
