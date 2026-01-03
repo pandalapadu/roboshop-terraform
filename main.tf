103fe3d06c0f9a373e8b0da4f5097468527f9294
VenkatReddy Kunam <venkat@gmail.com>
2026-01-03 16:32:29 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
-output "env" {
-  value = var.env
+module "components" {
+  for_each = var.components
+  source = "./modules/vm"
+  component = each.value["name"]
+  vm_size = each.value["vm_size"]
 }
