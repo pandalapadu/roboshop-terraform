7f0cff840a65c1e885541c9c1304b1e45a08b618
VenkatReddy Kunam <venkat@gmail.com>
2026-01-03 15:52:32 +0530

    Terraform

--- provider.tf
+++ provider.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name = "eCommerce"
+    storage_account_name = "azdevopsvenkat"
+    container_name = "tfstates"
+  }
 }
